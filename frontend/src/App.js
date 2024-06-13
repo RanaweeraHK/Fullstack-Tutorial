3966139d6c6904f941f49ee3ea9cbb7759ed7924
RanaweeraHK <ranaweerahk@gmail.com>
2024-06-14 02:08:14 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ContactList from "./ContactList";
+import "./App.css";
+import ContactForm from "./ContactForm";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
+
+  useEffect(() => {
+    fetchContacts()
+  }, []);
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchContacts()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+        </div>
+      </div>
+      }
+    </>
   );
 }
 
